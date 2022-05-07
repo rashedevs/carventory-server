5ceb2ead7a10ec1d0f0c92a5bbedb140a0f0b08f
rashedevs <rasheduap2015@gmail.com>
2022-05-07 14:05:21 +0600

    delete api implemented for delete a single data

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ async function run() {
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
+    //delete a specific data
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
